535e79b338e92f687de7c867617e3af86b711366
jasonchen <379762541@qq.com>
2026-06-25 13:38:53 +0800

    feat: scaffold Tauri + Vue 3 project with TailwindCSS, Milkdown, Pinia, sqlx

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import MainLayout from "./components/Layout/MainLayout";
-import "./App.css";
-
-function App() {
-  return <MainLayout />;
-}
-
-export default App;
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen bg-gray-950 text-white flex items-center justify-center">
+    <p class="text-lg">AiPen 加载中...</p>
+  </div>
+</template>
